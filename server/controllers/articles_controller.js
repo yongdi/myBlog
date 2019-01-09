8113aa6c78ad96144e5c6e0ec4b27fef0f6d6a65
Elson <weilesi@mozat.com>
2019-01-09 15:31:58 +0800

    stick first article on top

--- server/controllers/articles_controller.js
+++ server/controllers/articles_controller.js
@@ -67,15 +67,9 @@ export async function getAllArticles(ctx) {
             .skip(skip).catch(err => {
                 ctx.throw(500, '服务器内部错误');
             });
-        // 置顶第一篇文章
-        let stick = await Article.findById(new ObjectId('5b4eb7892eff2e0c1dc990a4')).catch(err => {
-            ctx.throw(500, '服务器内部错误');
-        });
-        articleArr.unshift(stick);
         allNum = await Article.count().catch(err => {
             this.throw(500, '服务器内部错误');
         });
-        allNum++;
     } else {
         let tagArr = tag.split(',');
         // console.log(tagArr)
@@ -125,11 +119,17 @@ export async function getAllPublishArticles(ctx) {
             .skip(skip).catch(err => {
                 ctx.throw(500, '服务器内部错误');
             });
+        // 置顶第一篇文章
+        let stick = await Article.findById(new ObjectId('5b4eb7892eff2e0c1dc990a4')).catch(err => {
+            ctx.throw(500, '服务器内部错误');
+        });
+        articleArr.unshift(stick);
         allNum = await Article.find({
             publish: true,
         }).count().catch(err => {
             this.throw(500, '服务器内部错误');
         });
+        allNum++;
     } else {
         let tagArr = tag.split(',');
         // console.log(tagArr)
