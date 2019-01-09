c005c501dda2c161a598f3f4b053a6fa48cf3443
Elson <weilesi@mozat.com>
2019-01-09 12:22:56 +0800

    article stick on top

--- server/controllers/articles_controller.js
+++ server/controllers/articles_controller.js
@@ -66,9 +66,15 @@ export async function getAllArticles(ctx) {
             .skip(skip).catch(err => {
                 ctx.throw(500, '服务器内部错误');
             });
+        // 置顶第一篇文章
+        let stick = await Article.findById('5b4eb7892eff2e0c1dc990a4').catch(err => {
+            ctx.throw(500, '服务器内部错误');
+        });
+        articleArr.unshift(stick);
         allNum = await Article.count().catch(err => {
             this.throw(500, '服务器内部错误');
         });
+        allNum++;
     } else {
         let tagArr = tag.split(',');
         // console.log(tagArr)
