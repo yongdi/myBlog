2304935ab75bff483880b9f00992d4ffc880c15d
Elson <weils@wifi.com>
2020-04-11 20:37:51 +0800

    输入回车也能搜索

--- client/src/modules/front/components/common/Side.vue
+++ client/src/modules/front/components/common/Side.vue
@@ -7,7 +7,7 @@
             <p class="sideBox__motto">full stack engineer</p>
             <p class="sideBox__motto">yongdi@qq.com</p>
 
-            <input type="text" placeholder="搜索标题..." v-model="sTitle" @blur="search(sTitle)">
+            <input type="text" placeholder="搜索标题..." v-model="sTitle" @blur="search(sTitle)" @keyup.enter="search(sTitle)">
             <ul class="sideBox__iconList">
                 <li v-for="icon in iconList" class="sideBox__iconItem" :key="icon.name">
                     <a :href="icon.href"><i class="iconfont" :class="'icon-'+icon.name"></i></a>
