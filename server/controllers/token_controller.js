00eece7e3dafdc9eba494629da9df61a48e5cde3
Elson <weils@zenmen.com>
2021-11-24 19:59:36 +0800

    add anti_crack to prevent brute force attack password

--- server/controllers/token_controller.js
+++ server/controllers/token_controller.js
@@ -2,15 +2,16 @@ import User from '../models/user.js';
 import md5 from 'md5';
 import jwt from 'jsonwebtoken';
 import config from '../configs/';
+const theCache = require('./cache');
 
 export async function initUser() {
     let user = await User.find().exec().catch(err => {
         console.log(err);
     });
     if (user.length === 0) {
-    // 目前还没做修改密码的功能，因为是单用户系统觉得需求不大
-    // 如果想更换用户名／密码，先将数据库原有user删除(drop)
-    // 配置中加入用户名密码，重启服务即可
+        // 目前还没做修改密码的功能，因为是单用户系统觉得需求不大
+        // 如果想更换用户名／密码，先将数据库原有user删除(drop)
+        // 配置中加入用户名密码，重启服务即可
         user = new User({
             name: 'hjm',
             username: config.admin.user,
@@ -27,6 +28,13 @@ export async function initUser() {
 export async function login(ctx) {
     const username = ctx.request.body.username;
     const password = ctx.request.body.password;
+
+    const fail_key = theCache.keyLogin + 'fail' + username,
+            ban_key = theCache.keyLogin + 'ban' + username
+
+    if (theCache.cache.has(ban_key))
+        ctx.throw(403, '请勿暴力破解!')
+
     let user = await User.findOne({
         username,
     }).exec();
@@ -44,9 +52,32 @@ export async function login(ctx) {
                 token: token,
             };
         } else {
+            anti_crack(fail_key, ban_key)
+
             ctx.throw(401, '密码错误');
         }
     } else {
+        anti_crack(fail_key, ban_key)
+
         ctx.throw(401, '用户名错误');
     }
+}
+
+/**
+ * 记录破解
+ * @param {type} fail_key
+ * @param {type} ban_key
+ * @returns {undefined}
+ */
+var anti_crack = (fail_key, ban_key) => {
+    // 增加失败次数
+    let fails = theCache.cache.get(fail_key)
+    if (!fails)
+        fails = 0
+    fails++
+    theCache.cache.set(fail_key, fails, 60)
+    
+    // 超过阈值，封禁10分钟
+    if (fails >= 3)
+        theCache.cache.set(ban_key, 1, 300)
 }
